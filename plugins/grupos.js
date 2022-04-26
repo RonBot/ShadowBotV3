5ba0fd5983bc01d9c1e408b8eb50ddcaccc33826
Roncito <96086954+RonBot@users.noreply.github.com>
2022-04-26 18:41:41 -0500

    Add files via upload

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,16 +3,12 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   
 let info = `
-*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Kan - Bot😎*
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Kan - Bot*
 
 *➤ Grupos oficiales del Bot:*
-*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+*1.-* https://chat.whatsapp.com/F7xxlnQ0VHg73IW5gJPtBA 
 
-*2.-* 🔥ENLACES 》https://chat.whatsapp.com/F7xxlnQ0VHg73IW5gJPtBA 
-
-*3.-* 😎AMISTAD 》https://chat.whatsapp.com/KHpBdTLe0K57qOIxdYxXs5 
-
-
+*2.-* https://chat.whatsapp.com/KHpBdTLe0K57qOIxdYxXs5
 `.trim() 
 
 conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 KAN - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
