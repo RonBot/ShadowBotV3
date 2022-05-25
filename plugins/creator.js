0c86fc22494dc584b28f3f870af3830f798cf8f0
Roncito <96086954+RonBot@users.noreply.github.com>
2022-05-24 20:08:45 -0500

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,5 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '573023900508', 'Kanixxx Creador - Oficial 1', m)
-this.sendContact(m.chat, '50488899999', 'Kanixxx - Creador - Oficial 2', m)}
+this.sendContact(m.chat, '573023900508', 'Kanixxx Creador - Oficial ', m)}
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
