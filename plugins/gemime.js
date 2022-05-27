7e51f8ab9441f724f5275a635c74ebf5f2b12566
Roncito <96086954+RonBot@users.noreply.github.com>
2022-05-27 17:05:05 -0500

    Add files via upload

--- plugins/gemime.js
+++ plugins/gemime.js
@@ -2,8 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/gemime.mp3'
-conn.sendFile(m.chat, vn, 'gemime.mp3', null, m, true, {
+let vn = './media/gemime.m4a'
+conn.sendFile(m.chat, vn, 'gemime.m4a', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
