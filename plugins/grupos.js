8afa05729c9a8f213531244fd22a5c633c0ad0c5
Roncito <96086954+RonBot@users.noreply.github.com>
2022-05-29 19:45:15 -0500

    Add files via upload

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,17 +3,17 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   
 let info = `
-*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Kan - Bot*
+*Ola $(username) 😎, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Kan - Bot*
 
 *➤ Grupos oficiales del Bot:*
 *1.-* https://chat.whatsapp.com/F7xxlnQ0VHg73IW5gJPtBA 
 
 *2.-* https://chat.whatsapp.com/KHpBdTLe0K57qOIxdYxXs5
 
-*2.-* https://chat.whatsapp.com/HguDoHEsQ4iLjVfKKnaWpx
+*3.-* https://chat.whatsapp.com/HguDoHEsQ4iLjVfKKnaWpx
 `.trim() 
 
-conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 KAN - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
+conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 𝑲𝑨𝑵 - 𝑩𝑶𝑻 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
 
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
 module.exports = handler
