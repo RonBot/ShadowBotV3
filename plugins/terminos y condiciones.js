d0225cdde255b2a5e1d9da57ee2a023b423609e7
Roncito <96086954+RonBot@users.noreply.github.com>
2022-05-31 17:13:02 -0500

    Add files via upload

--- plugins/terminos y condiciones.js
+++ plugins/terminos y condiciones.js
@@ -21,7 +21,7 @@ _2.- No nos hacemos responsables de la ignorancia y/o desconocimiento del tema_
 _3.- El Bot no esta activo las 24 horas al menos de que el propietario decida lo contrario_
 _4.- La "empresa" no se hace responsable del uso de un Sub Bot o Bot no oficial, ya que son personas (terceros) y no tenemos control sobre ell@s_
 _5.- No nos hacemos responsables de los números que se puedan ir a soporte por el uso del Bot, es aconsejable siempre usar números virtuales para usos de Bots_
-_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de The Shadow Brokers - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
+_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de Kan - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
 
 *➤ Mensaje del numero del Bot?*
 
@@ -45,13 +45,13 @@ _- Si en algún momento recibe un mensaje del numero del Bot y no es un comando 
 *⁉️ Te quedaron dudas?*
 
 _- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_ 
-> wa.me/5219996125657
-> Bruno Sobrino 
-> Correo electronico: theshadowbrokers133@gmailcom
+> wa.me/573023900508
+> Kanixxx 
 
-→ *Solo contactar por temas serios, para preguntas del Bot mas generales contáctame al privado con el comando del comando #owner (al numero wa.me/17722386341)*
 
-*∆ THE SHADOW BROKERS - BOT ∆*
+→ *Solo contactar por temas serios, para preguntas del Bot mas generales contáctame al privado con el comando del comando #owner (al numero wa.me/573023900508)*
+
+*∆  「 𝑲𝑨𝑵 - 𝑩𝑶𝑻  」 ∆*
 `.trim(), m)
 let mentionedJid = [m.sender]} 
 handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i
