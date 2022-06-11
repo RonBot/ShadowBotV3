928ecdefe77134a2c0c427e30b21be9dab3ab9f0
Roncito <96086954+RonBot@users.noreply.github.com>
2022-06-10 23:06:09 -0500

    Add files via upload

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'          
 heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Hentai By ShadowBot*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+conn.sendButtonImg(m.chat, json, '*Hentai By Kan Bot*', '©𝑲𝑨𝑵 - 𝑩𝑶𝑻🌙', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
 module.exports = handler
